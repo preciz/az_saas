7e50ec80cea5fbc5dd198dc8f7d61974f963d9ec
preciz <barna.kovacs.codes@gmail.com>
2020-04-12 13:13:21 +0200

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AzSaas.MixProject do
     [
       app: :az_saas,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -13,16 +13,14 @@ defmodule AzSaas.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 1.6"},
+      {:jason, "~> 1.0"}
     ]
   end
 end
